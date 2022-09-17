6f428e6bb78fd6720a4ed6cffaea79aa28fe477d
pidokige02 <pidokige0204@gmail.com>
2022-09-17 16:53:09 +0900

    #2.3 Server Side Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +34,17 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// this function is conduted only in the server.
+// return된 results 는 home function 의 input prop 으로 들어가게 된다.
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
